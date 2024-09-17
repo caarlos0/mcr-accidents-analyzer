6e9c46ac5a3e96814c6dff495ed972a553f6276b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-09-17 14:21:24 -0300

    first
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Web scraper for opresente.com.br";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+        pythonWithPackages = python.withPackages (ps: with ps; [
+          requests
+          beautifulsoup4
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pythonWithPackages
+          ];
+          shellHook = ''
+            echo "Web scraper development environment"
+            echo "Python version: $(python --version)"
+            echo "Available packages: requests, beautifulsoup4"
+          '';
+        };
+      }
+    );
+}
